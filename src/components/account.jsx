1c9499fc5962573ade91002068d63cdd414d25bb
Pallavi18042004 <pallavimahanandi204@gmail.com>
2024-09-24 16:24:20 +0530

    hii

---
+++ src/components/account.jsx
@@ -0,0 +1,36 @@
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './App.css'
+
+
+function App (){
+
+    return( <div>
+
+    <div className='bg-slate-950'>
+        <input type='Search' placeholder='Search'></input>
+        <h4 className='font-semibold text-white text-xl'>Simplewood</h4>
+        <nav> <ul> 
+        <li> Home </li>
+        <li> Products </li>
+        <li> Elements </li>
+        <li> Pages </li>
+        <li> Shop </li>
+        <li> Sale </li>
+
+        </ul>
+        </nav>
+
+
+    </div>
+
+
+    <div><img src=""></img></div>
+
+    </div>
+
+    )
+}
+export default App;

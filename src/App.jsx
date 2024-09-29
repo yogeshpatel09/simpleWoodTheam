64c1ec61eac0e5d771ec32cabae4c8e8d4feb1b4
yogeshpatel09 <yogeshpatel7028@gmail.com>
2024-09-29 21:31:04 +0530

    delete files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
-import Account from './components/Account'
 import Home from './components/Home'
 function App() {
   
 
   return (
     <>
-      <Account/>
+      <Home/>
     </>
   )
 }
